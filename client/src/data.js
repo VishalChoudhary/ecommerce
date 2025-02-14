55147bad00fb86938f88c99b1a264baee5793729
VishalChoudhary <vishalghn953@gmail.com>
2025-02-14 17:08:18 +0530

    react slider

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "/images/shopping1.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "E9EDF0",
+    },
+    {
+      id: 2,
+      img: "/images/shopping2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "FCF1ED",
+    },
+    {
+      id: 3,
+      img: "/images/shopping3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "F4E1F5",
+    },
+  ];
