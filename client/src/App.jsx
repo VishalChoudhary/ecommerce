fa8cbcf10af36ad45df7264cdb21488eb0570745
VishalChoudhary <vishalghn953@gmail.com>
2025-02-15 18:18:10 +0530

    single product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line
 import Home from "./pages/Home";
+// eslint-disable-next-line
 import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 const App = () => {
   return <div>
-    <ProductList />
+    <Product />
   </div>;
 };
 
