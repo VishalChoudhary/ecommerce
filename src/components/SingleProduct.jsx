8489adcc45c2c6fb0a05540479e846077062097c
VishalChoudhary <vishalghn953@gmail.com>
2025-02-15 12:28:50 +0530

    styled component hover & animation

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -2,24 +2,8 @@ import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@m
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  flex: 1;
-  margin: 5px;
-  min-width: 280px;
-  height: 350px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5fbfd;
-  position: relative;
-`;
-
-const Image = styled.img`
-  height: 100%;
-  z-index: 2;
-`;
-
 const Info = styled.div`
+  opacity: 0;
   width: 100%;
   height: 100%;
   position: absolute;
@@ -30,6 +14,28 @@ const Info = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  transition: all 0.5s ease;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+  
+  &:hover ${Info}{
+    opacity: 1;
+  }
+`;
+
+const Image = styled.img`
+  height: 100%;
+  z-index: 2;
 `;
 
 const Icon = styled.div`
@@ -42,6 +48,7 @@ const Icon = styled.div`
   justify-content: center;
   margin: 10px;
   transition: all 0.5s ease;
+  cursor: pointer;
 
   &:hover{
     background-color: #e9f5f5;
