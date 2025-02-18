4dd73e34117a50250237f9612785c24a0bb85170
VishalChoudhary <vishalghn953@gmail.com>
2025-02-18 13:34:20 +0530

    mongoDB connection setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 
+//database connection
+require('./db');
 
-app.listen(5000,(req,res)=>{
+app.listen(process.env.PORT || 5000,()=>{
     console.log("Server started at 5000");
 });
