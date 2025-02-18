ec9a6e58a1b7c0d82cc5f7109701f080f9919b94
VishalChoudhary <vishalghn953@gmail.com>
2025-02-18 23:43:15 +0530

    nodejs router

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const userRoute = require('./routes/user');
 
 //database connection
 require('./db');
 
-app.listen(process.env.PORT || 5000,()=>{
+app.use(express.json());
+
+app.use('/api/users',userRoute); 
+
+app.listen(process.env.PORT || 5000 ,()=>{
     console.log("Server started at 5000");
 });
