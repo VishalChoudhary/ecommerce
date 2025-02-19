06d397487bca27e7502d53ef449b3d6806e1e879
VishalChoudhary <vishalghn953@gmail.com>
2025-02-19 22:45:11 +0530

    mongoDB authentication

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 //database connection
 require('./db');
 
 app.use(express.json());
 
+app.use('/api/auth',authRoute);
 app.use('/api/users',userRoute); 
 
 app.listen(process.env.PORT || 5000 ,()=>{
---
+++ server/routes/auth.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+//REGISTER
+router.post("/register", async(req,res)=>{
+    const {username, email, password} = req.body;
+
+    try{
+        // Hash password before saving to DB
+        const salt = await bcrypt.genSalt(16);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        //create new user
+        const newUser = new User({
+            username,
+            email,
+            password:hashedPassword,
+        });
+
+        //save user in DB
+        const savedUser = await newUser.save();
+        const { password:_ , ...userData} = savedUser._doc;
+        res.status(201).json(userData);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+
+router.post('/login',async(req,res)=>{
+    const {username, password} = req.body;
+    try{
+        //find user in DB
+        const user = await User.findOne({username});
+        if(!user)
+            return res.status(401).json("Wrong Credentials!");
+
+        //compare passwords using bcrypt
+        const validPassword = await bcrypt.compare(password,user.password);
+        if(!validPassword)
+            return res.status(401).json("Wrong Credentials!");
+
+        const { password:_ , ...userData} = user._doc;
+        res.status(201).json(userData);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
