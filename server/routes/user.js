07cff9592827153dfeae349afc8fbc6a670b04f6
VishalChoudhary <vishalghn953@gmail.com>
2025-02-22 22:02:52 +0530

    jwt authentication & verification

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,26 @@
+const User = require('../models/User');
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
+
 const router = require('express').Router();
 
-router.get('/usertest',(req,res)=>{
-    res.send("user test is successful");
-});
-
-router.post('/userposttest',(req,res)=>{
-    const username = req.body;
-    console.log(username);
-});
+router.put('/:id', verifyTokenAndAuthorization, async(req,res)=>{
+    if(req.body.password){
+        //If a password update is requested, it is hashed using bcrypt before storing it in the database.
+        const salt = await bcrypt.genSalt(16);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try{
+        const updateUser = await User.findByIdAndUpdate(req.params.id,
+            {
+            $set: req.body
+            },
+            {new:true}
+        );
+        res.status(200).json(updateUser);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
