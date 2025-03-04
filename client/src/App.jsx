3088022b10d82f2e674c0df9ffdaf16d3b9804f7
VishalChoudhary <vishalghn953@gmail.com>
2025-03-05 02:00:11 +0530

    react-router-dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
 import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
+import Cart from "./pages/Cart";
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
-  return <div>
-    <Register />
-  </div>;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        {/* Redirecting if user is logged in */}
+        <Route path="/login" element={user ? <Navigate to ="/" /> :<Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
