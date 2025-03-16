894207a6d310bf06cce0f4a54eea53078ab00762
VishalChoudhary <vishalghn953@gmail.com>
2025-03-16 23:01:59 +0530

    navbar fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import { Badge } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { logout } from "../redux/userRedux"; // Import logout action
+import { logout } from "../redux/userRedux"; 
 
 const Container = styled.div`
   height: 60px;
@@ -75,7 +75,7 @@ const DropdownMenu = styled.div`
   background: white;
   border: 1px solid #ccc;
   padding: 10px;
-  width: 150px;
+  width: 200px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
   z-index: 100;
